799eed1e4a0cd9bd63553220a67e8d181f91a400
cc <276307854@qq.com>
2017-03-13 16:08:53 +0800

    First commit.

---
+++ lbstest/src/main/java/com/cc/lbstest/MainActivity.java
@@ -0,0 +1,126 @@
+package com.cc.lbstest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class MainActivity extends AppCompatActivity {
+
+    public LocationClient mLocationClient = null;
+    private TextView positionText;
+    private MapView mapView;
+    private BaiduMap baiduMap;
+    private boolean isFirstLocate = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //声明LocationClient类
+        mLocationClient = new LocationClient(getApplicationContext());
+        //注册监听函数
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.bmapView);
+        baiduMap = mapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
+        positionText = (TextView) findViewById(R.id.position_text_view);
+        requestLocation();
+    }
+
+    private void requestLocation() {
+        initLocation();
+        mLocationClient.start();
+    }
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        //设置更新间隔
+        option.setScanSpan(5000);
+        //设置精度模式
+//        option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);
+        //设置我们需要获取位置的详细信息
+        option.setIsNeedAddress(true);
+        mLocationClient.setLocOption(option);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stop();
+        mapView.onDestroy();
+        baiduMap.setMyLocationEnabled(false);
+    }
+
+    private class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation || bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
+                navigateTo(bdLocation);
+            }
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition.append("纬度:").append(bdLocation.getLatitude()).append("\n");
+            currentPosition.append("经度:").append(bdLocation.getLongitude()).append("\n");
+            currentPosition.append("国家:").append(bdLocation.getCountry()).append("\n");
+            currentPosition.append("省:").append(bdLocation.getProvince()).append("\n");
+            currentPosition.append("市:").append(bdLocation.getCity()).append("\n");
+            currentPosition.append("区:").append(bdLocation.getDistrict()).append("\n");
+            currentPosition.append("街道:").append(bdLocation.getStreet()).append("\n");
+            currentPosition.append("定位方式:");
+            if (bdLocation.getLocType() == BDLocation.TypeCacheLocation) {
+                currentPosition.append("GPS");
+            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
+                currentPosition.append("网络.....");
+            }
+            positionText.setText(currentPosition);
+        }
+
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+
+        }
+
+        private void navigateTo(BDLocation bdLocation) {
+            if (isFirstLocate) {
+                LatLng ll = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+                MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+                baiduMap.animateMapStatus(update);
+                update = MapStatusUpdateFactory.zoomTo(16f);
+                baiduMap.animateMapStatus(update);
+                isFirstLocate = false;
+            }
+            MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
+            locationBuilder.latitude(bdLocation.getLatitude());
+            locationBuilder.longitude(bdLocation.getLongitude());
+            MyLocationData locationData = locationBuilder.build();
+            baiduMap.setMyLocationData(locationData);
+        }
+    }
+
+
+}
